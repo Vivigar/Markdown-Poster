c5711f2ed57705bbabb529d9e6af273145ba0ab2
Jordan Carney <Jrdnkcarney@gmail.com>
2020-01-15 00:36:17 -0500

    Posts can be submitted and an index value is added to the right of each post

--- js/markdown-parser.js
+++ js/markdown-parser.js
@@ -1,18 +1,33 @@
-console.log("Initiating Markdown Parser");
+console.log("Initiating Post Maker");
 
 let textArea = document.getElementById('text-parse-area');
-console.log(textArea);
+let submitButton = document.getElementById('submit-button');
+let postCounter = 1;
 
-textArea.addEventListener('keyup', autoSize);
-
-function autoSize() {
-    textArea.style.height = "auto";
-    textArea.style.padding = "none;"
-
-    textArea.style.height = textArea.scrollHeight + "px";
+// Makes sure the height is set to automatic and reassigns it based on the scroll height of the text box.
+function resizeTextArea() {
+    textArea.style.cssText = "height: auto; padding: none;"; // Set height to auto first so that the height won't adjust unless neccisary
+    textArea.style.height = textArea.scrollHeight + "px"; 
 }
 
-function autoSizeTextArea() {
+textArea.addEventListener('keyup', resizeTextArea);
+submitButton.addEventListener('click', submitThePost);
 
+// Fetches the post counter and the content of the text area to assign it in HTML code as a string value
+function getThePost() {
+    let postIDHTML = '<div class="postID">' + postCounter + '</div>';
+    let postContentHTML = '<div class="postContent">' + textArea.value + '</div> ';
+    return '<div class="post">' + postIDHTML + postContentHTML + '</div>';
+}
 
+function submitThePost() {
+    let newPost = document.createElement("div").innerHTML = getThePost();
+    textArea.insertAdjacentHTML('beforebegin', newPost);
+
+    // Reset the value of the text area and brings the focus back
+    textArea.value = '';
+    textArea.focus();
+
+    // Increment post counter for the list
+    postCounter++;
 }
